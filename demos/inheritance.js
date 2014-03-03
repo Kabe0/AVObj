f70e1293726c81cb52cf2294656357324e9e6c4d
Ian Carson <ian@verticaldepth.ca>
2014-03-03 00:23:01 -0800

    Added the first demo. Realized that what I thought was a bug was really just the way Javascript handles objects.

---
+++ demos/inheritance.js
@@ -0,0 +1,11 @@
+/**
+ * User: Ian
+ * Date: 01/03/14
+ * Time: 7:15 PM
+ * Copyright (c) 2014 Ian Carson - LinkedIn http://lnkd.in/9qJWap
+ * License : MIT (http://www.opensource.org/licenses/mit-license.php)
+ *
+ * This tutorial is to show how to handle extending AVObjects and inheriting attributes and methods from parent objects.
+ * In addition the tutorial will also show road blocks that may occur in relation to the "parent" property.
+ */
+
